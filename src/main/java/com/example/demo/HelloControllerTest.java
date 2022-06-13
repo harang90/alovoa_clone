8787aa957fe6f157f5b7bd36fd5787cc8cc62e3a
harang90 <harang90@gmail.com>
2022-06-13 22:15:35 +0900

    [WIP] WIP WIP

--- src/main/java/com/example/demo/HelloControllerTest.java
+++ src/main/java/com/example/demo/HelloControllerTest.java
@@ -1,6 +1,8 @@
 package com.example.demo;
 
-import static org.hamcret.Matchers.equalTo;
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,9 +11,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
