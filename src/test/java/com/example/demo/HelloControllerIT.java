3e8658250bec20c51c874be14a16cafa6e9a0365
harang90 <harang90@gmail.com>
2022-06-14 21:43:23 +0900

    [WIP] wip

--- src/test/java/com/example/demo/HelloControllerIT.java
+++ src/test/java/com/example/demo/HelloControllerIT.java
@@ -6,6 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class HelloControllerIT {
 
@@ -17,5 +19,4 @@ public class HelloControllerIT {
         ResponseEntity<String> response = template.getForEntity("/", String.class);
         assertThat(response.getBody()).isEqualTo("Greetings from Spring Boot!");
     }
-
 }
